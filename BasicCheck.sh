b831eb6e9c349f040cc19ffe5fae2d20e09cab4f
shimon1705 <44582779+shimon1705@users.noreply.github.com>
2019-03-11 17:00:11 +0200

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -12,37 +12,47 @@ if [ $? -eq 0 ]; then
 else
     Compil=1
 
-    valgrind --tool=memcheck ${@:3} --leak-check=full --error-exitcode=1 -q ./$execute 
+ valgrind --tool=helgrind --error-exitcode=1 -q ./$execute &> /dev/null
+    if [ $? -eq 0 ]; then 
+       TreadTemp=0
+    else
+       TreadTemp=1
+    fi
+
+    valgrind --tool=memcheck ${@:3} --leak-check=full --error-exitcode=1 -q ./$execute &> /dev/null
     if [ $? -eq 0 ]; then
        MemoLa=0
     else
        MemoLa=1
     fi
  
-    valgrind --tool=helgrind --error-exitcode=1 -q ./$execute 
-    if [ $? -eq 0 ]; then 
-       TreadTemp=0
-    else
-       TreadTemp=1
-    fi
+   
 fi
 
-answer=$Compil$MemoLa$TreadTemp
-
-if [ $answer == '000' ]; then
-    echo "Compilation succes  Memory leaks succes Thread succes"
+if [ $Compil$MemoLa$TreadTemp == '000' ]; then
+    echo "Compilation ok "
+    echo "Memory leaks ok "
+    echo "Thread ok"
     exit 0
-elif [ $answer == '001' ]; then
-      echo "Compilation succes   Memory leaks succes   Thread FAIL   "
+elif [ $Compil$MemoLa$TreadTemp == '001' ]; then
+      echo "Compilation ok "
+      echo "Memory leaks ok "
+      echo "Thread FAIL"
       exit 1
-elif [ $answer == '010' ]; then
-      echo "Compilation- ucces   Memory leaks FAIL   Thread succes  "
+elif [ $Compil$MemoLa$TreadTemp == '010' ]; then
+      echo "Compilation ok "
+      echo "Memory leaks FAIL "
+      echo "Thread ok  "
       exit 2
-elif [ $answer == '011' ];then 
-      echo "Compilation succes   Memory leaks FAIL   Thread Fail"
+elif [ $Compil$MemoLa$TreadTemp == '011' ];then 
+      echo "Compilation ok "
+      echo "Memory leaks FAIL "
+      echo "Thread Fail"
       exit 3
 else 
-      echo "Compilation FAIL   Memory leaks FAIL   Thread FAIL "
+      echo "Compilation FAIL "
+      echo "Memory leaks FAIL "
+      echo "Thread FAIL "
       exit 7
 fi
 
