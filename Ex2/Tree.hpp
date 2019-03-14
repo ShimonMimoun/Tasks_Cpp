c8631dfa5b8f4064aacbc08849629419466d548c
shimon1705 <44582779+shimon1705@users.noreply.github.com>
2019-03-14 14:23:32 +0200

    Add files via upload

---
+++ Ex2/Tree.hpp
@@ -0,0 +1,20 @@
+
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+
+
+ BST();
+  
+    void insert(int x);
+    bool remove(int x);
+    bool contains(int x);
+    int root();
+    int parent (int x)
+    int size();
+    int left(int x);
+    int right(int x);
+
+
